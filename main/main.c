e15fb5915eb324bbdbb44a14f04098edbab781ae
meeeerkat <meeeerkat@protonmail.com>
2022-03-05 05:59:25 +0100

    Modified project name & removed some useless comments

--- main/main.c
+++ main/main.c
@@ -1,15 +1,7 @@
-/* This example code is in the Public Domain (or CC0 licensed, at your option.)
-   Unless required by applicable law or agreed to in writing, this software is
-   distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
- */
-
 #include "esp_system.h"
 #include "nvs_flash.h"
 #include "codelock.h"
 #include "validation_leds.h"
-
-static const char *TAG = "BHOCKZ";
 
 void codelock_success_callback(void) {
     blink_validation_led(SUCCESS_LED);
